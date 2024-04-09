1e7d2747d7fca0368dc63d8879f6cb6cf662a5ce
Geil625 <152264083+Geil625@users.noreply.github.com>
2024-04-09 16:06:46 +0800

    Create LD_block.sh

---
+++ LD_block.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+pig=... # the pig individual under consideration
+SNPdata=... # the plink .bim .bam .fam files of SNP data
+
+cd .../$pig
+plink --bfile $SNPdata \
+ --chr 1,4,18,23 \
+ --blocks no-pheno-req \
+ --blocks-max-kb 1000 \
+ --blocks-strong-lowci 0.5001 \
+ --blocks-strong-highci 1 \
+ --out LD_block
